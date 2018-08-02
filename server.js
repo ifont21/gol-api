7f1e525a2421b9756671949c10bf6d0042d94d40
ignacio <igfont21@gmail.com>
2018-08-01 22:04:35 -0500

    up to heroku

--- server.js
+++ server.js
@@ -5,13 +5,11 @@ const app = express();
 const defaultPort = process.env.PORT || 8080;
 
 app.get('/hello', (req, res) => {
-    res.send('hello world');
+    res.send({ message: 'hello world' });
 });
 
 app.listen(defaultPort, async () => {
-    if (process.env.NODE_ENV === 'development') {
-        console.log(`Navigate to http://localhost:${defaultPort} to use the application`);
-    }
+    console.log(`Navigate to http://localhost:${defaultPort} to use the application`);
 });
 
 module.exports = app;
